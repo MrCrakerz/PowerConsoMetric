217b19d766b098cb08698bfae9b2d5d5e4c05c9c
Your Name <you@example.com>
2022-03-16 20:57:23 +0100

    Mise en place du premier Graphique représentatif

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -63,36 +63,90 @@ class UploadController extends AbstractController
 
         $objets = json_decode(file_get_contents('../var/uploads/bd.json'));
 
-        $nomMesureGlobale="Consommation Globale";
+        $tableauHote=array();
+        $compteurNuméroMesure=0;
+
+        foreach ($objets as $mesure) {
+          $tableauHote= $tableauHote + array(
+            $compteurNuméroMesure => array(
+              'conso'=>$mesure->host->consumption,
+              'date'=>$mesure->host->timestamp,
+            )
+          );
+          $compteurNuméroMesure++;
+        }
+
+        $tableauConso=array();
+        $compteurNuméroMesure=0;
+        foreach ($objets as $mesure) {
+          $consommateurs = $mesure->consumers;
+          foreach ($consommateurs as $consommateur) {
+            $tableauConso= array_merge($tableauConso, array (
+                $compteurNuméroMesure=>$consommateur->exe,
+            )
+          );
+            $compteurNuméroMesure++;
+          }
+        }
+        $tableauFinal =array();
+        for ($i=0;$i<sizeof($tableauConso)-1;$i++){
+          $cpt=0;
+          foreach ($objets as $mesure){
+            foreach ($mesure->consumers as $appli){
+              if ($tableauConso[$i]==$appli->exe){
+              $tableauFinal=array_merge_recursive($tableauFinal,array(
+                  $cpt=> array (
+                      'name'=>$appli->exe,
+                    'conso'=>$appli->consumption,
+                    'heure'=>$appli->timestamp,
+                  )
+              ));
+              $cpt ++;
+            }
+          }
+        }
+        }
+
+
+        $json = json_encode($tableauHote);
+        $bytes = file_put_contents("../var/uploads/verif.json", $json);
+        $json = json_encode($tableauFinal);
+        $bytes = file_put_contents("../var/uploads/verif2.json", $json);
+
+/*        $nomMesureGlobale="Consommation Globale";
         $consoMesureGlobale=0.0;
-        $dureeMesureGlobale=0;
-
-
+        $dureeMesureGlobale=array();
+        $i=0;
         foreach ($objets as $data) {
               $hote = $data->host;
               $consommateurs = $data->consumers;
-              if (isset($hote->consumption))
-              {
               $consoMesureGlobale=$consoMesureGlobale+$hote->consumption;
-              }
-              if (isset($hote->timestamp))
-              $dureeMesureGlobale=$dureeMesureGlobale+$hote->timestamp;
-
-              $consommateurs = $data->consumers;
+              $dureeMesureGlobale=$dureeMesureGlobale+array(
+                $i=> $hote->timestamp,
+              );
+              $i++;
+              $consommate<urs = $data->consumers;
               $tableauConso = Array();
+              $j=0;
               foreach ($consommateurs as $consommateur) {
+                $conso =$consommateur->consumption;
+                $conso = $conso * pow(10,-6);
                 $tableauConso=$tableauConso+Array(
-                    $consommateur->exe=> array (
+                     $consommateur->exe=> array (
                       'nom'=>$consommateur->exe,
-                      'conso'=>$consommateur->consumption,
-                      'duree'=>$consommateur->timestamp,
+                      'conso'=> array(
+                        $j=>$consommateur->timestamp,
+                      )   //MicroWatt
                     )
                   );
+                  $j++;
               }
               $json = json_encode($tableauConso);
-$bytes = file_put_contents("../var/uploads/verif.json", $json);
+              $bytes = file_put_contents("../var/uploads/verif.json", $json);
         }
+        $consoMesureGlobale = $consoMesureGlobale * pow(10,-6);
+        $dureeMesureGlobale =$dureeMesureGlobale[sizeof($dureeMesureGlobale)-1]-$dureeMesureGlobale[0];*/
 
-        return $this->render('home/dashboard.html.twig',['nomMesureGlobale'=>$nomMesureGlobale,'consoMesureGlobale'=>$consoMesureGlobale,'dureeMesureGlobale'=>$dureeMesureGlobale,'tabConso'=>$tableauConso],);
+        return $this->render('home/dashboard.html.twig',['tableauHote'=>$tableauHote,'tableauConso'=>$tableauConso,'tableauFinal'=>$tableauFinal],);
     }
 }
