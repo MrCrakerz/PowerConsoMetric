ffeaf873ed01b63b95a26dc70c0c9a056cafbe43
Your Name <you@example.com>
2022-04-05 12:18:40 +0200

    Retirer automatiquement les blocs vides

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -61,12 +61,9 @@ class UploadController extends AbstractController
         $filename = $file->getClientOriginalName();
         $uploader->upload($uploadDir, $file, $filename);
 
-
         $objets = json_decode(file_get_contents('../var/uploads/bd.json'));
-
-        $tableauHote=array();
         $compteurNuméroMesure=0;
-
+        $tableauHote=array();
         foreach ($objets as $mesure) {
           $tableauHote= $tableauHote + array(
             $compteurNuméroMesure => array(
@@ -76,7 +73,6 @@ class UploadController extends AbstractController
           );
           $compteurNuméroMesure++;
         }
-
         $tableauConso=array();
         $compteurNuméroMesure=0;
         foreach ($objets as $mesure) {
@@ -98,7 +94,6 @@ class UploadController extends AbstractController
             );
               $compteurNuméroMesure++;
             }
-
           }
         }
         $tableauFinal =array();
@@ -121,46 +116,13 @@ class UploadController extends AbstractController
         }
 
 
-        $json = json_encode($tableauHote);
+        /*$json = json_encode($tableauHote);
         $bytes = file_put_contents("../var/uploads/verif.json", $json);
         $json = json_encode($tableauConso);
         $bytes = file_put_contents("../var/uploads/verif3.json", $json);
         $json = json_encode($tableauFinal);
-        $bytes = file_put_contents("../var/uploads/verif2.json", $json);
+        $bytes = file_put_contents("../var/uploads/verif2.json", $json);*/
 
-/*        $nomMesureGlobale="Consommation Globale";
-        $consoMesureGlobale=0.0;
-        $dureeMesureGlobale=array();
-        $i=0;
-        foreach ($objets as $data) {
-              $hote = $data->host;
-              $consommateurs = $data->consumers;
-              $consoMesureGlobale=$consoMesureGlobale+$hote->consumption;
-              $dureeMesureGlobale=$dureeMesureGlobale+array(
-                $i=> $hote->timestamp,
-              );
-              $i++;
-              $consommate<urs = $data->consumers;
-              $tableauConso = Array();
-              $j=0;
-              foreach ($consommateurs as $consommateur) {
-                $conso =$consommateur->consumption;
-                $conso = $conso * pow(10,-6);
-                $tableauConso=$tableauConso+Array(
-                     $consommateur->exe=> array (
-                      'nom'=>$consommateur->exe,
-                      'conso'=> array(
-                        $j=>$consommateur->timestamp,
-                      )   //MicroWatt
-                    )
-                  );
-                  $j++;
-              }
-              $json = json_encode($tableauConso);
-              $bytes = file_put_contents("../var/uploads/verif.json", $json);
-        }
-        $consoMesureGlobale = $consoMesureGlobale * pow(10,-6);
-        $dureeMesureGlobale =$dureeMesureGlobale[sizeof($dureeMesureGlobale)-1]-$dureeMesureGlobale[0];*/
 
         return $this->render('home/dashboard.html.twig',['tableauHote'=>$tableauHote,'tableauConso'=>$tableauConso,'tableauFinal'=>$tableauFinal],);
     }
